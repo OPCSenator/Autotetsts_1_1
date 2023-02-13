a5073150a823cb4f88efe130be44694a7ada19c3
OPCSenator <opc_senator4@mail.ru>
2023-02-14 00:19:33 +0300

    testng

--- src/test/java/ru/netology/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/CashBackHackServiceTest.java
@@ -1,17 +1,20 @@
 package ru.netology;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.testng.Assert.assertEquals;
 
 class CashBackHackServiceTest {
 
-    @Test
-    void remain() {
+    @org.testng.annotations.Test
+     void remain() {
         int amount = 900;
         int expected = 100;
         CashBackHackService cashBack = new CashBackHackService();
         int actual = cashBack.remain(amount);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
